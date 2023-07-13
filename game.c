6b2b2e801c402bcd29f0c9045ad4b9c8678fb88f
bitiutsm <mbitiutskyy@gmail.com>
2023-07-13 16:25:25 +0100

    Rewrite input handler

--- game.c
+++ game.c
@@ -9,36 +9,40 @@ int parsed_value;
 
 
 void mode_select(int value) {
-    if (parsed_value == 1) {
-        // Start Player vs Player
-        printf("Starting Player vs Player...\n");
-        //player_vs_player();
+    switch(parsed_value) {
+        case 1:
+            printf("Player vs Player\n");
+            //start Player vs Player game
+            break;
+        case 2:
+            printf("Player vs CPU\n");
+            //start Player vs CPU game
+            break;
+        case 3:
+            printf("Exiting the application...\n");
+            // close the application
+            exit(0);
+        default:
+            printf("???\n");
+            break;
     }
-    else if (parsed_value == 2) {
-        // Start Player vs CPU
-        printf("Starting Player vs CPU...\n");
-        //player_vs_cpu();
-    }
-}
-
-int input_evaluate(char *input) {
-    while(!(sscanf(input, "%d", &parsed_value) == 1 && parsed_value < 3 && parsed_value > 0)) {
-        printf("\nInvalid input. Try again\n");
-        printf("Type the corresponding number: ");
-        fgets(input, sizeof(input), stdin);
-    }
-    printf("Input: %d\n", parsed_value);
-    return parsed_value;
 }
 
 void play_game() {
     printf("Choose the game mode:\n");
     printf("1. Player vs Player\n");
     printf("2. Player vs CPU\n");
+    printf("3. Exit the application\n");
     printf("Type the corresponding number: ");
-    fgets(input, sizeof(input), stdin);
 
-    parsed_value = input_evaluate(input);
+    while (1) {
+        fgets(input, sizeof(input), stdin);
+        if (sscanf(input, "%d", &parsed_value) == 1 && parsed_value >= 1 && parsed_value <= 3) {
+            break;
+        }
+        printf("\nInvalid input. Try again\n");
+        printf("Type the corresponding number: ");
+    }
 
     mode_select(parsed_value);
 }
