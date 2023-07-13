315ce1d5c7ec28de39ab94ce1353efb25754931f
bitiutsm <mbitiutskyy@gmail.com>
2023-07-13 16:25:40 +0100

    Update header file

--- game.h
+++ game.h
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
-#define MAX_LENGTH 100
+#define MAX_LENGTH 256
 
 void mode_select(int value);
-
-int invalid_input();
 
 void play_game();
