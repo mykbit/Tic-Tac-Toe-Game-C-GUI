4e69adf3b2a7d622d9d323ca4bb6bf1a5265cbc6
bitiutsm <mbitiutskyy@gmail.com>
2023-07-14 18:24:12 +0100

    Change main for testing purposes

--- main.c
+++ main.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 #include <gtk/gtk.h>
 #include "start_game.h"
+#include "window.h"
 
 int main(int argc, char* argv[]) {
 
     gtk_init(&argc, &argv);
-    play_game();
+    //play_game();
+
+    open_window();
 
     return 0;
 }
