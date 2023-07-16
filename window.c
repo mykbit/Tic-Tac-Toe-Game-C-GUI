53e8e5ce791bde5941421c99e96b6251e35602b4
bitiutsm <mbitiutskyy@gmail.com>
2023-07-16 16:16:34 +0100

    Add Restart button

--- window.c
+++ window.c
@@ -1,47 +1,60 @@
 #include "window.h"
 
-gchar *current_turn_string = "Player 1's Turn";
-gchar *start_game_string = "Game Start!";
-
-
-void open_window() {
-    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+void create_game_window() {
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_default_size(GTK_WINDOW(window), 1000, 600);
     gtk_window_set_title(GTK_WINDOW(window), "Tic Tac Toe");
-    g_signal_connect(window, "delete_event", G_CALLBACK(quit_app), NULL);
+    window_handler_id = g_signal_connect(window, "delete_event", G_CALLBACK(quit_app), NULL);
 
-    GtkWidget *exit_button = gtk_button_new_with_label("Exit");
-    g_signal_connect(exit_button, "clicked", G_CALLBACK(quit_app), NULL);
+    current_turn_string = "Player 1's Turn";
+    start_game_string = "Game Start!";
+
+    exit_button = gtk_button_new_with_label("Exit");
+    exit_button_handler_id = g_signal_connect(exit_button, "clicked", G_CALLBACK(quit_app), NULL);
+
+    restart_btn = gtk_button_new_with_label("Restart");
+    restart_button_handler_id = g_signal_connect(restart_btn, "clicked", G_CALLBACK(restart_game), NULL);
+    gtk_widget_set_sensitive(restart_btn, FALSE);
+
     
     score_lbl = gtk_label_new("");
     draw_score(score_lbl, X, O);
 
-    GtkWidget *turn_lbl = gtk_label_new("Game Start!");
+    turn_lbl = gtk_label_new("Game Start!");
 
     // create a grid to hold the score and exit button
     game_grid = gtk_grid_new();
 
     // create buttons 3 by 3
-    GtkWidget *btn11 = gtk_button_new_with_label("\u00B7");
-    GtkWidget *btn12 = gtk_button_new_with_label("\u00B7");
-    GtkWidget *btn13 = gtk_button_new_with_label("\u00B7");
-    GtkWidget *btn21 = gtk_button_new_with_label("\u00B7");
-    GtkWidget *btn22 = gtk_button_new_with_label("\u00B7");
-    GtkWidget *btn23 = gtk_button_new_with_label("\u00B7");
-    GtkWidget *btn31 = gtk_button_new_with_label("\u00B7");
-    GtkWidget *btn32 = gtk_button_new_with_label("\u00B7");
-    GtkWidget *btn33 = gtk_button_new_with_label("\u00B7");
+    btn11 = gtk_button_new_with_label("\u00B7");
+    btn12 = gtk_button_new_with_label("\u00B7");
+    btn13 = gtk_button_new_with_label("\u00B7");
+    btn21 = gtk_button_new_with_label("\u00B7");
+    btn22 = gtk_button_new_with_label("\u00B7");
+    btn23 = gtk_button_new_with_label("\u00B7");
+    btn31 = gtk_button_new_with_label("\u00B7");
+    btn32 = gtk_button_new_with_label("\u00B7");
+    btn33 = gtk_button_new_with_label("\u00B7");
 
     // create signal handlers for the buttons
-    g_signal_connect(btn11, "clicked", G_CALLBACK(button_click_callback), turn_lbl);
-    g_signal_connect(btn12, "clicked", G_CALLBACK(button_click_callback), turn_lbl);
-    g_signal_connect(btn13, "clicked", G_CALLBACK(button_click_callback), turn_lbl);
-    g_signal_connect(btn21, "clicked", G_CALLBACK(button_click_callback), turn_lbl);
-    g_signal_connect(btn22, "clicked", G_CALLBACK(button_click_callback), turn_lbl);
-    g_signal_connect(btn23, "clicked", G_CALLBACK(button_click_callback), turn_lbl);
-    g_signal_connect(btn31, "clicked", G_CALLBACK(button_click_callback), turn_lbl);
-    g_signal_connect(btn32, "clicked", G_CALLBACK(button_click_callback), turn_lbl);
-    g_signal_connect(btn33, "clicked", G_CALLBACK(button_click_callback), turn_lbl);
+    btn11_handler_id = g_signal_connect(btn11, "clicked", G_CALLBACK(button_click_callback), turn_lbl);
+    g_object_set_data(G_OBJECT(btn11), "id", "00");
+    btn12_handler_id = g_signal_connect(btn12, "clicked", G_CALLBACK(button_click_callback), turn_lbl);
+    g_object_set_data(G_OBJECT(btn12), "id", "01");
+    btn13_handler_id = g_signal_connect(btn13, "clicked", G_CALLBACK(button_click_callback), turn_lbl);
+    g_object_set_data(G_OBJECT(btn13), "id", "02");
+    btn21_handler_id = g_signal_connect(btn21, "clicked", G_CALLBACK(button_click_callback), turn_lbl);
+    g_object_set_data(G_OBJECT(btn21), "id", "10");
+    btn22_handler_id = g_signal_connect(btn22, "clicked", G_CALLBACK(button_click_callback), turn_lbl);
+    g_object_set_data(G_OBJECT(btn22), "id", "11");
+    btn23_handler_id = g_signal_connect(btn23, "clicked", G_CALLBACK(button_click_callback), turn_lbl);
+    g_object_set_data(G_OBJECT(btn23), "id", "12");
+    btn31_handler_id = g_signal_connect(btn31, "clicked", G_CALLBACK(button_click_callback), turn_lbl);
+    g_object_set_data(G_OBJECT(btn31), "id", "20");
+    btn32_handler_id = g_signal_connect(btn32, "clicked", G_CALLBACK(button_click_callback), turn_lbl);
+    g_object_set_data(G_OBJECT(btn32), "id", "21");
+    btn33_handler_id = g_signal_connect(btn33, "clicked", G_CALLBACK(button_click_callback), turn_lbl);
+    g_object_set_data(G_OBJECT(btn33), "id", "22");
 
     // make widgets expand to fill the grid
     gtk_widget_set_hexpand(btn11, TRUE);
@@ -83,22 +96,48 @@ void open_window() {
     gtk_widget_set_vexpand(score_lbl, TRUE);
     gtk_widget_set_hexpand(turn_lbl, TRUE);
     gtk_widget_set_vexpand(turn_lbl, TRUE);
+    gtk_widget_set_hexpand(restart_btn, TRUE);
+    gtk_widget_set_vexpand(restart_btn, TRUE);
 
     gtk_grid_attach(GTK_GRID(main_grid), turn_lbl,    0, 0, 2, 1);
     gtk_grid_attach(GTK_GRID(main_grid), game_grid,   0, 1, 1, 10);
-    gtk_grid_attach(GTK_GRID(main_grid), score_lbl,   1, 1, 1, 7);
+    gtk_grid_attach(GTK_GRID(main_grid), score_lbl,   1, 1, 1, 5);
+    gtk_grid_attach(GTK_GRID(main_grid), restart_btn, 1, 7, 1, 2);
     gtk_grid_attach(GTK_GRID(main_grid), exit_button, 1, 9, 1, 2);  
 
     gtk_container_add(GTK_CONTAINER(window), main_grid);
     gtk_widget_show_all(window);
     g_timeout_add_seconds(3, game_start_text, turn_lbl);
+}
+
+void open_window() {
+    create_game_window();
     gtk_main();
-    return;
 }
 
 void quit_app(GtkWidget *widget, gpointer ptr) {
     gtk_main_quit();
-    return;
+}
+
+void restart_game(GtkWidget *widget, gpointer ptr) {
+    turn_count = 0;
+    clean_matrix();
+    g_signal_handler_disconnect(window, window_handler_id);
+    g_signal_handler_disconnect(exit_button, exit_button_handler_id);
+    g_signal_handler_disconnect(restart_btn, restart_button_handler_id);
+    g_signal_handler_disconnect(btn11, btn11_handler_id);
+    g_signal_handler_disconnect(btn12, btn12_handler_id);
+    g_signal_handler_disconnect(btn13, btn13_handler_id);
+    g_signal_handler_disconnect(btn21, btn21_handler_id);
+    g_signal_handler_disconnect(btn22, btn22_handler_id);
+    g_signal_handler_disconnect(btn23, btn23_handler_id);
+    g_signal_handler_disconnect(btn31, btn31_handler_id);
+    g_signal_handler_disconnect(btn32, btn32_handler_id);
+    g_signal_handler_disconnect(btn33, btn33_handler_id);
+
+    gtk_widget_destroy(window);
+    gtk_main_quit();
+    open_window();
 }
 
 // Callback function to change the label text
@@ -127,7 +166,6 @@ void button_click_callback(GtkWidget *widget, gpointer ptr) {
     gtk_widget_set_sensitive(widget, FALSE);
     GtkWidget *turn = GTK_WIDGET(ptr);
 
-
     const gchar *turn_text = gtk_label_get_text(GTK_LABEL(turn));
     
     if (g_strcmp0(turn_text, current_turn_string) == 0 || g_strcmp0(turn_text, start_game_string) == 0) {
@@ -152,34 +190,30 @@ void apply_turn(GtkWidget *widget, gpointer ptr) {
         X++;
         gtk_label_set_text(GTK_LABEL(ptr), "Player 1 Wins!");
         gtk_widget_set_sensitive(game_grid, FALSE);
+        gtk_widget_set_sensitive(restart_btn, TRUE);
     }
     else if (result == 1 && current_symbol == 'O') {
         O++;
         gtk_label_set_text(GTK_LABEL(ptr), "Player 2 Wins!");
         gtk_widget_set_sensitive(game_grid, FALSE);
+        gtk_widget_set_sensitive(restart_btn, TRUE);
     }
     else if (result == 0 && turn_count == 9) {
         gtk_label_set_text(GTK_LABEL(ptr), "Draw!");
         gtk_widget_set_sensitive(game_grid, FALSE);
+        gtk_widget_set_sensitive(restart_btn, TRUE);
     }
     draw_score(score_lbl, X, O);
     
 }
 
 void apply_widget_position(GtkWidget *widget) {
-    GtkAllocation allocation;
+    char *button_id = g_object_get_data(G_OBJECT(widget), "id");
 
-    // Get the allocation of the widget
-    gtk_widget_get_allocation(widget, &allocation);
+    int x = button_id[0] - '0';
+    int y = button_id[1] - '0';
 
-    // Access the position of the widget
-    gint x = allocation.x;
-    gint y = allocation.y;
-
-    // Print the position of the widget
-    //printf("Widget position: x = %d, y = %d\n", x, y);
-
-    matrix[x / 183][y / 181] = current_symbol;
+    matrix[x][y] = current_symbol;
 }
 
 int check_win() {
@@ -208,4 +242,12 @@ int check_win() {
     }
 
     else return -1; // No winning combination found yet
+}
+
+void clean_matrix() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            matrix[i][j] = ' ';
+        }
+    }
 }
--- window.h
+++ window.h
@@ -7,12 +7,42 @@ int O;
 char current_symbol;
 int turn_count;
 char matrix[3][3];
+GtkWidget *window;
+GtkWidget *exit_button;
 GtkWidget *score_lbl;
 GtkWidget *game_grid;
+GtkWidget *restart_btn;
+GtkWidget *turn_lbl;
+gchar *current_turn_string;
+gchar *start_game_string;
+GtkWidget *btn11;
+GtkWidget *btn12;
+GtkWidget *btn13;
+GtkWidget *btn21;
+GtkWidget *btn22;
+GtkWidget *btn23;
+GtkWidget *btn31;
+GtkWidget *btn32;
+GtkWidget *btn33;
+
+gulong window_handler_id;
+gulong exit_button_handler_id;
+gulong restart_button_handler_id;
+gulong btn11_handler_id;
+gulong btn12_handler_id;
+gulong btn13_handler_id;
+gulong btn21_handler_id;
+gulong btn22_handler_id;
+gulong btn23_handler_id;
+gulong btn31_handler_id;
+gulong btn32_handler_id;
+gulong btn33_handler_id;
 
 void open_window();
 
 void quit_app(GtkWidget *widget, gpointer ptr);
+
+void restart_game(GtkWidget *widget, gpointer ptr);
 
 gboolean game_start_text(gpointer user_data);
 
@@ -29,3 +59,5 @@ void apply_widget_position(GtkWidget *widget);
 void print_matrix();
 
 int check_win();
+
+void clean_matrix();
