345b029663b39e2e33b1c718e8745ea8e7000cbf
bitiutsm <mbitiutskyy@gmail.com>
2023-07-26 14:21:41 +0100

    Add comments in .h files

--- start_game.h
+++ start_game.h
@@ -7,6 +7,8 @@
 extern int sel_mode;
 char input[MAX_LENGTH];
 
+// Selects the game mode based on the user input
 void mode_select();
 
+// Accepts the user input and calls the function to select the game mode
 void play_game();
